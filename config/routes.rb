de066d81ced5a1e93e895a7dcceabaa3189c2ab1
frankburke333 <frank.burke@gmail.com>
2017-02-07 15:30:42 -0500

    add user views, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # root 'users#index'
+  resources :reservations
+  resources :users, only: [:new, :create]
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
