8efd6de2e51efd4d33dc4e33425c9b06f8471ebf
g-mehra <gmehra@alumni.uwo.ca>
2017-02-09 01:16:54 -0500

    Add show page for each reservation option (no confirm functionality yet)

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,12 +4,15 @@ class RestaurantsController < ApplicationController
       time = params[:time].to_s
       @time = time[0..1].to_i
       @party_size = params[:party_size].to_i
+      session[:current_res_date] = @date
+      session[:current_res_time] = @time
+      session[:current_party_size] = @party_size 
 
       # Sum's # of reservations for a particular time and date
       @valid_restaurants = []
 
       Restaurant.all.each do |restaurant|
-    
+
         @capacity_taken = Reservation.where(date: @date, time: @time, restaurant_id: restaurant.id).sum(:party_size)
         if @party_size <= (restaurant.capacity - @capacity_taken)
           @valid_restaurants << restaurant
