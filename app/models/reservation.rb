fcc8e8c9e3c32be3e6c98c8532c8c0b6121016b5
frankburke333 <frank.burke@gmail.com>
2017-02-09 18:25:09 -0500

    add first attempt at form validation, commented out in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,11 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
 
   validates :time, :date, :party_size, :restaurant_id, :name, :email, presence: true
+  # validate :capacity_exists
+  #
+  # private
+  # def capacity_exists
+  #   session[:party_size] < session[:capacity_left]
+  # end
+
 end
