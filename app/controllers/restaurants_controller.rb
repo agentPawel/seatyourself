bd71ec680908c9c9b7a9a422e15f3806fcd76105
g-mehra <gmehra@alumni.uwo.ca>
2017-02-09 01:33:03 -0500

    Got reservation data to be stored in session hash - allows for user to go back from restaurant reservation page without losing data

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
-      @date = params[:date].to_s
-      time = params[:time].to_s
-      @time = time[0..1].to_i
-      @party_size = params[:party_size].to_i
-      session[:current_res_date] = @date
-      session[:current_res_time] = @time
-      session[:current_party_size] = @party_size 
+      session[:current_res_date] = params[:date].to_s
+      #@date = params[:date].to_s
+
+      session[:current_res_time] = params[:time][0..1].to_i
+      #time = params[:time].to_s
+      #@time = time[0..1].to_i
+      session[:party_size] = params[:party_size].to_i
+      #@party_size = params[:party_size].to_i
+
 
       # Sum's # of reservations for a particular time and date
       @valid_restaurants = []
 
       Restaurant.all.each do |restaurant|
 
-        @capacity_taken = Reservation.where(date: @date, time: @time, restaurant_id: restaurant.id).sum(:party_size)
-        if @party_size <= (restaurant.capacity - @capacity_taken)
+        @capacity_taken = Reservation.where(date: session[:current_res_date], time: session[:current_res_time], restaurant_id: restaurant.id).sum(:party_size)
+        if session[:party_size] <= (restaurant.capacity - @capacity_taken)
           @valid_restaurants << restaurant
         end
       end
